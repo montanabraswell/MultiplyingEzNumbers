f5a3f42116868e32e77a8449b7fe7df67a71347c
montanabraswell <montanabraswell1@gmail.com>
2017-11-06 21:30:43 -0500

    Add files via upload
    Multiplying ez numbers; Java Tutorial 7

---
+++ Multiplication/src/multiplication/Multiplication.java
@@ -0,0 +1,46 @@
+/* MOOC Java Tutorial 7
+ * Java Tutorial 7
+ * Montana Braswell
+ * 11/3/2017
+ */
+package multiplication;
+
+/**
+ *
+ * @author Montana
+ */
+public class Multiplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int x;
+        int y;
+        int answer;
+        
+        x = 2;
+        y = 8;
+        answer = x * y;
+        System.out.println(answer);
+        System.out.println(x + " * " + y + " = " + answer);
+       
+        
+        int a;
+        int b;
+        int result;
+        
+        a = 277;
+        b = 111;
+        result = a * b;
+        System.out.println(a + " * " + b + " = " + result);
+        
+        
+    }
+    
+    
+    
+
+}
